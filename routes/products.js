fc80e9cc23bf2e340820ad5c0fd5dec577a38a04
Nik Atlas <nikatlas@gmail.com>
2017-11-10 16:22:34 +0200

    products by day implementation + DB update

--- routes/products.js
+++ routes/products.js
@@ -3,27 +3,39 @@ var router = new express.Router();
 
 var knex = require('../db/db.js');
 
+function filterDate(data) {
+  var day = new Date().getDay();
+  var newArr = [];
+  for(var i=0;i<data.length;i++){
+    var days = JSON.parse(data[i].Days);
+    if(days[day])newArr.push(data[i]);
+  }
+  return newArr;
+}
+
 /* GET users listing. */
 router.get('/', (req, res) => {
-  knex.table('products').select('*').
-    then((data) => res.send(data));
+  knex.table('products').select('*')
+    .then(filterDate)
+    .then((data) => res.send(data));
 });
 
 router.get('/category/:catid', (req, res) => {
-  knex.table('products').select('*').
-    where({ category_id: req.params.catid }).
-    then((data) => res.send(data));
+  knex.table('products').select('*')
+    .where({ category_id: req.params.catid })
+    .then(filterDate)
+    .then((data) => res.send(data));
 });
 
 router.get('/:id', (req, res) => {
-  knex.table('products').select('*').
-    where({ id: req.params.id }).
-    then((data) => res.send(data[0]));
+  knex.table('products').select('*')
+    .where({ id: req.params.id })
+    .then((data) => res.send(data[0]));
 });
 
 router.post('/', (req, res) => {
-  knex.table('products').insert(req.body).
-    then((data) => res.send(data));
+  knex.table('products').insert(req.body)
+    .then((data) => res.send(data));
 });
 
 module.exports = router;
