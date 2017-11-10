d09c61e67d524ad51bac6538a07d7669ce495b3c
Nik Atlas <nikatlas@gmail.com>
2017-11-10 16:54:50 +0200

    Fix warnings

--- routes/products.js
+++ routes/products.js
@@ -20,27 +20,27 @@ function filterDate(data) {
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  knex.table('products').select('*')
-    .then(filterDate)
-    .then((data) => res.send(data));
+  knex.table('products').select('*').
+      then(filterDate).
+      then((data) => res.send(data));
 });
 
 router.get('/category/:catid', (req, res) => {
-  knex.table('products').select('*')
-    .where({ category_id: req.params.catid })
-    .then(filterDate)
-    .then((data) => res.send(data));
+  knex.table('products').select('*').
+    where({ category_id: req.params.catid }).
+    then(filterDate).
+    then((data) => res.send(data));
 });
 
 router.get('/:id', (req, res) => {
-  knex.table('products').select('*')
-    .where({ id: req.params.id })
-    .then((data) => res.send(data[0]));
+  knex.table('products').select('*').
+    where({ id: req.params.id }).
+    then((data) => res.send(data[0]));
 });
 
 router.post('/', (req, res) => {
-  knex.table('products').insert(req.body)
-    .then((data) => res.send(data));
+  knex.table('products').insert(req.body).
+    then((data) => res.send(data));
 });
 
 module.exports = router;
