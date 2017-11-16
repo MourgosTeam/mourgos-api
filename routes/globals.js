7c2c496dcbdfdc593b610028efacfdbb7de853ce
Nik Atlas <nikatlas@gmail.com>
2017-11-16 18:31:31 +0200

    Fix warnings

--- routes/globals.js
+++ routes/globals.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var router = new express.Router();
 var knex = require('../db/db.js');
+
 /* GET users listing. */
 router.get('/', (req, res) => {
   knex.table('globals').select('*').
-		then((data) => {
-			var result = {};
-			for(var i=0;i<data.length;i++){
-				result[data[i].Name] = data[i].Value;
-			}
-			res.send(result);
-		});
+  then((data) => {
+    var result = {};
+    for (var ic = 0; ic < data.length; ic += 1) {
+      result[data[ic].Name] = data[ic].Value;
+    }
+    res.send(result);
+  });
 });
 router.get('/id/:id', (req, res) => {
   knex.table('globals').select('*').
 where({ id: req.params.id }).
 then((data) => res.send(data[0]));
 });
- 
+
 router.get('/:name', (req, res) => {
   knex.table('globals').select('*').
 where({ FriendlyURL: req.params.name }).
