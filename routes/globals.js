08c33a56b0ce9bbc2a27292ffe03d61d9d02bc8a
Nik Atlas <nikatlas@gmail.com>
2018-02-12 13:31:26 +0200

    WorkingHours

--- routes/globals.js
+++ routes/globals.js
@@ -30,4 +30,41 @@ router.post('/', (req, res) => {
 then((data) => res.send(data));
 });
 
+router.get('/mourgos/status', (req, res) => {
+  knex.table('globals').select('*').
+  where({ Name: 'MourgosIsLive' }).
+  orWhere({ Name: 'MourgosHasProblem' }).
+  orWhere({ Name: 'MourgosWorkingHours' }).
+  then((data) => {
+    const globals = data.
+    reduce((initial, item) => {
+      initial[item.Name] = item.Value;
+
+      return initial;
+    }, {});
+    const hours = globals.MourgosWorkingHours.split('-');
+    const today = new Date();
+    const now = (today.getHours() < 10
+      ? '0'
+      : '') + today.getHours() + ':' +
+    (today.getMinutes() < 10
+      ? '0'
+      : '') + today.getMinutes();
+
+    const isWorking = hours[0] <= now && now <= hours[1]
+                    ? 1
+                    : 0;
+
+    return isWorking ||
+    Math.max(globals.MourgosIsLive, 2 * globals.MourgosHasProblem);
+  }).
+  then((status) => res.send({ Status: status }));
+});
+
+router.post('/change/workingHours', (req, res) => {
+  const times = req.body.value;
+  knex.table('globals').where({ Name: 'MourgosWorkingHours' }).
+  update({ Value: times }).
+  then(() => res.send(true));
+});
 module.exports = router;
